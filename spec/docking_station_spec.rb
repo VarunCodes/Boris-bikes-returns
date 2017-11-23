ea8d25d4f06e9cbe9c9cc3d9fe5a05a48a661e58
tom <tomallpress@hotmail.com>
2017-11-23 15:51:40 +0000

    Challenge 12: Created an error when no bikes exist.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,40 +2,44 @@ require "docking_station"
 
 describe DockingStation do
 
-  # it { is_expected.to respond_to(:dock).with(1).argument }
-  # it { is_expected.to respond_to :release_bike}
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  it { is_expected.to respond_to :release_bike}
 
   describe '#release_bike' do
     it 'releases a bike' do
       bike = Bike.new
       d = DockingStation.new
       d.dock(bike)
-      p subject.release_bike
-      p subject.release_bike
       expect(d.release_bike).to eq bike
+    end
+
+
+    it 'raises an error when there are no bikes in the docking station' do
+      d = DockingStation.new
+      expect {subject.release_bike}.to raise_error 'No bikes available'
     end
   end
 
 
-  # it 'should get a bike' do
-  #   bike = subject.release_bike
-  #   expect(bike).to be_working
-  # end
-  #
-  # #it {is_expected.to respond_to(:dock).with(1).argument}
-  #
-  # it 'docks something' do
-  #   bike = Bike.new
-  #   expect(subject.dock(bike)).to eq bike
-  # end
-  #
-  # #it {is_expected.to respond_to(:bike)}
-  #
-  # it 'returns docked bike' do
-  #   bike = Bike.new
-  #   subject.dock(bike)
-  #   expect(subject.bike).to eq bike
-  # end
+  it 'should get a bike' do
+    bike = Bike.new
+    expect(bike.working?).to eq true
+  end
+
+  #it {is_expected.to respond_to(:dock).with(1).argument}
+
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  #it {is_expected.to respond_to(:bike)}
+
+  it 'returns docked bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
 
 
 
