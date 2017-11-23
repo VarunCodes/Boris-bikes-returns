99cdc91421b8eb82144aff7016a803e2475e5b1a
Varun Madhukara <mvarun@hotmail.co.uk>
2017-11-23 15:26:59 +0000

    Challenge 12: Dock and retrieve bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,32 +1,45 @@
 require "docking_station"
 
 describe DockingStation do
-  #test with basic structure
-  # it 'has a method called release_bike' do
-  #   docking_station = DockingStation.new
-  #   expect(docking_station).to respond_to(:release_bike)
+
+  # it { is_expected.to respond_to(:dock).with(1).argument }
+  # it { is_expected.to respond_to :release_bike}
+
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      d = DockingStation.new
+      d.dock(bike)
+      p subject.release_bike
+      p subject.release_bike
+      expect(d.release_bike).to eq bike
+    end
+  end
+
+
+  # it 'should get a bike' do
+  #   bike = subject.release_bike
+  #   expect(bike).to be_working
+  # end
+  #
+  # #it {is_expected.to respond_to(:dock).with(1).argument}
+  #
+  # it 'docks something' do
+  #   bike = Bike.new
+  #   expect(subject.dock(bike)).to eq bike
+  # end
+  #
+  # #it {is_expected.to respond_to(:bike)}
+  #
+  # it 'returns docked bike' do
+  #   bike = Bike.new
+  #   subject.dock(bike)
+  #   expect(subject.bike).to eq bike
   # end
 
-  #test with one liner
-  it { is_expected.to respond_to :release_bike}
 
-  it 'should get a bike' do
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
 
-  #it {is_expected.to respond_to(:dock).with(1).argument}
-
-  it 'docks something' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
-  end
-
-  #it {is_expected.to respond_to(:bike)}
-
-  it 'returns docked bike' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
-  end
 end
+
+
+#expect { ... }.to throw_symbol(:symbol, 'value')
